bd51adce54871a13962a990d80ab8ea1d960e7ad
Piero <piero.onorati@virgilio.it>
2021-07-08 16:49:46 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <img src="../assets/img/spotify-icon-marilyn-scott-0.png" alt="logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+
+}
+</script>
+
+<style lang='scss'>
+@import '../style/mixins.scss';
+
+header{
+    @include glass;
+
+    img{
+        width: 50px;
+    }
+}
+
+</style>
+
+
+
---
+++ src/components/MainContent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h2>ciao</h2>
+        <!-- <Card/> -->
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+// import Card from '@/components/Card.vue'
+
+export default {
+    name:'MainContent',
+    components:{
+        // Card,
+    },
+    data(){
+        return{
+            apiURL : 'https://flynn.boolean.careers/exercises/api/array/music',
+            records:[]
+        }
+    },
+    created(){
+        this.getTheRecord()
+    },
+
+    methods:{
+        getTheRecord(){
+            axios
+                .get(this.apiURL)
+                .then(res=>{
+                    this.records= res.data.response;
+                })
+                .catch(error =>{
+                    console.log('Errore', error);
+                });
+
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
