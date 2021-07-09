a8e6406a639711834fa61126d1b57253c7b37d5f
Piero <piero.onorati@virgilio.it>
2021-07-09 21:42:53 +0200

    add select for the date

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,12 +1,21 @@
 <template>
     <section>
-        <div class="container-sm py-4">
-            <div class="col-3 d-flex">
-            <select class="form-select" aria-label="Default select example" v-model="selected">
-                <option>All</option>
-                <option v-for="(option,index) in options" :key="index">{{option}}</option>
-            </select>
-            </div>
+        <div class="container-sm pb-4">
+            <div class="col-6 select py-4">
+                <span class="px-3">Select genre...</span>
+                <select v-model="selected">
+                    <option >All</option>
+                    <option v-for="(option,index) in options" :key="index">{{option}}</option>
+                </select>
+
+                <span class="px-3">Sort by date...</span>
+                <select v-model="dateSelected">
+                    <option>Random</option>
+                    <option>Newest</option>
+                    <option>Oldest</option>
+                </select>
+            </div>   
+           
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 gx-4" v-if="!loading">
                 <Card v-for="(record,index) in filterRecords" :key="index" :details="record"/>
             </div>
@@ -34,7 +43,9 @@ export default {
             records:[],
             loading : true,
             selected:'',
+            dateSelected:'',
             options:[],
+            new:[]
         }
     },
 
@@ -44,27 +55,24 @@ export default {
     },
 
     computed:{
-        filterRecords(){
-         
-                return this.records.filter(element =>{
-                    if (this.selected == '' || this.selected == 'All') {
-                        return this.records
-                        
-                    }else{
-                        
-                        return element.genre == this.selected
 
-                    }
-            });
-                
-            
-            
-            
-
+        filterRecords: function(){
+            return this.filterByDate(this.filterByGenre(this.records))
         }
-        
-        
-        
+
+        // filterRecords(){
+
+        //     return this.records.filter(element =>{
+        //         if (this.selected == '' || this.selected == 'All') {
+        //             return this.records
+        //         }
+        //         else{
+        //             return element.genre == this.selected
+        //         }
+        //     });
+
+        // }
+
     },
 
     methods:{
@@ -90,11 +98,35 @@ export default {
             });
         },
 
-      
+        filterByGenre(array){
+            return array.filter(element =>{
+                if (this.selected == '' || this.selected == 'All') {
+                    return array
+                }
+                else{
+                    return element.genre == this.selected
+                }
+            });
+        },
 
-        
+        filterByDate(array){
+            if (this.dateSelected =='Newest') {
+                array = array.sort((a,b) => {
+                   return b.year- a.year 
+                })
+                return array
+                
+            }else if(this.dateSelected =='Oldest'){
+                array = array.sort((a,b) => {
+                   return a.year - b.year
+                })
+                return array
 
-    
+            }else{
+                return array
+            }
+            
+        }
     }
 
 
@@ -107,6 +139,19 @@ export default {
 section{
     background-color: $dark;
     height: 100vh;
+    overflow-y:scroll ;
+    .select{
+        color:$text-light;
+
+        select{
+            padding: 5px;
+            background-color: lightgrey;
+            width: 100px;
+        } 
+
+    }
+  
+  
 }
 
 
